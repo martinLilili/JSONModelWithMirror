60f7a2f3fa55a652dcfcd6378e95e0792d6353fd
martin <martin.li@ubtrobot.com>
2016-12-14 21:12:49 +0800

    将类和方法设置为public

--- JSONModel/JSONModel.swift
+++ JSONModel/JSONModel.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 //MARK: - MirrorResult，使用mirror遍历所有的属性和值，通过action传出做相应的处理
-protocol MirrorResult {
+public protocol MirrorResult {
     
     /// 使用mirror遍历所有的属性和值
     ///
@@ -19,9 +19,9 @@ protocol MirrorResult {
     func getResultFromMirror(mir : Mirror, action: (_ label: String?, _ value : Any) -> Void)
 }
 
-extension MirrorResult {
+public extension MirrorResult {
     
-    func getResultFromMirror(mir : Mirror, action: (_ label: String?, _ value : Any) -> Void) {
+    public func getResultFromMirror(mir : Mirror, action: (_ label: String?, _ value : Any) -> Void) {
         if let superMirror = mir.superclassMirror { //便利父类所有属性
             getResultFromMirror(mir: superMirror, action: action)
         }
@@ -36,7 +36,7 @@ extension MirrorResult {
 
 //MARK: - JSON协议
 //自定义一个JSON协议
-protocol JSON: MirrorResult {
+public protocol JSON: MirrorResult {
     
     /// 如果是对象实现了JSON协议，这个方法返回对象属性及其value的dic，注意如果某个value不是基础数据类型，会一直向下解析直到基础数据类型为止，另外可选类型和数组需要单独处理
     /// 如果是基础数据类型实现了JSON协议，只返回他们自己
@@ -50,34 +50,34 @@ protocol JSON: MirrorResult {
 }
 
 //扩展协议方法
-extension JSON {
+public extension JSON {
     
     
     /// 使用mirror遍历所有的属性，并保存与dic中，如果属性的value不是基础类型，则一直向下解析直到基础类型，另外可选类型和数组需要单独处理
     ///
     /// - Parameter mir: mirror
     /// - Returns: dic
-//    func getResultFromMirror(mir : Mirror) -> [String:AnyObject] {
-//        var result: [String:AnyObject] = [:]
-//        if let superMirror = mir.superclassMirror { //便利父类所有属性
-//            result = getResultFromMirror(mir: superMirror)
-//        }
-//        if (mir.children.count) > 0  {
-//            for case let (label?, value) in (mir.children) {
-//                //属性：label   值：value
-//                if let jsonValue = value as? JSON { //如果value实现了JSON，继续向下解析
-//                    result[label] = jsonValue.toJSONModel()
-//                }
-//            }
-//        }
-//        return result
-//    }
+    //    func getResultFromMirror(mir : Mirror) -> [String:AnyObject] {
+    //        var result: [String:AnyObject] = [:]
+    //        if let superMirror = mir.superclassMirror { //便利父类所有属性
+    //            result = getResultFromMirror(mir: superMirror)
+    //        }
+    //        if (mir.children.count) > 0  {
+    //            for case let (label?, value) in (mir.children) {
+    //                //属性：label   值：value
+    //                if let jsonValue = value as? JSON { //如果value实现了JSON，继续向下解析
+    //                    result[label] = jsonValue.toJSONModel()
+    //                }
+    //            }
+    //        }
+    //        return result
+    //    }
     
     /// 将数据转成可用的JSON模型
-    func toJSONModel() -> AnyObject? {
+    public func toJSONModel() -> AnyObject? {
         let mirror = Mirror(reflecting: self)
         if mirror.children.count > 0  {
-//            let result = getResultFromMirror(mir: mirror)
+            //            let result = getResultFromMirror(mir: mirror)
             var result: [String:AnyObject] = [:]
             getResultFromMirror(mir: mirror, action: { (label, value) in
                 //属性：label   值：value
@@ -91,7 +91,7 @@ extension JSON {
     }
     
     //将数据转成JSON字符串
-    func toJSONString() -> String {
+    public func toJSONString() -> String {
         
         let jsonModel = self.toJSONModel()
         //利用OC的json库转换成OC的Data，
@@ -108,7 +108,7 @@ extension JSON {
 //扩展可选类型，使其遵循JSON协议
 extension Optional: JSON {
     //可选类型重写toJSONModel()方法
-    func toJSONModel() -> AnyObject? {
+    public func toJSONModel() -> AnyObject? {
         if let x = self {
             if let value = x as? JSON {
                 return value.toJSONModel()
@@ -124,7 +124,7 @@ extension Int: JSON { }
 extension Bool: JSON { }
 extension Dictionary: JSON { }
 extension Array: JSON {
-    func toJSONModel() -> AnyObject? {
+    public func toJSONModel() -> AnyObject? {
         let mirror = Mirror(reflecting: self)
         if mirror.children.count > 0  {
             var arr:[Any] = []
@@ -143,7 +143,7 @@ extension Array: JSON {
 
 
 //得到所有的属性即value
-protocol PropertieValues: MirrorResult {
+public protocol PropertieValues: MirrorResult {
     /// 得到所有的属性及其对应的value组成dic，注意value不一定全是基础数据类型，可能是其他自定义对象。同时，dic中存储的都是有值的属性，那些没有赋值的属性不会出现在dic中
     ///
     /// - Returns: dic
@@ -154,26 +154,26 @@ protocol PropertieValues: MirrorResult {
 }
 
 //MARK: - PropertieValues协议
-extension PropertieValues {
-//    func getPropertieValuesWithMirror(mir : Mirror) -> [String:AnyObject] {
-//        var result: [String:AnyObject] = [:]
-//        if let superMirror = mir.superclassMirror {
-//            result = getPropertieValuesWithMirror(mir: superMirror)
-//        }
-//        for case let (label?, value) in (mir.children) {
-//            result[label] = unwrap(any: value) as AnyObject?
-//        }
-//        return result
-//    }
-//    
+public extension PropertieValues {
+    //    func getPropertieValuesWithMirror(mir : Mirror) -> [String:AnyObject] {
+    //        var result: [String:AnyObject] = [:]
+    //        if let superMirror = mir.superclassMirror {
+    //            result = getPropertieValuesWithMirror(mir: superMirror)
+    //        }
+    //        for case let (label?, value) in (mir.children) {
+    //            result[label] = unwrap(any: value) as AnyObject?
+    //        }
+    //        return result
+    //    }
+    //
     
     /// 得到所有的属性及其对应的value组成dic，注意value不一定全是基础数据类型，可能是其他自定义对象。同时，dic中存储的都是有值的属性，那些没有赋值的属性不会出现在dic中
     ///
     /// - Returns: dic
-    func codablePropertieValues() -> [String:AnyObject] {
+    public func codablePropertieValues() -> [String:AnyObject] {
         var codableProperties = [String:AnyObject]()
         let mirror = Mirror(reflecting: self)
-//        codableProperties = getPropertieValuesWithMirror(mir: mirror)
+        //        codableProperties = getPropertieValuesWithMirror(mir: mirror)
         getResultFromMirror(mir: mirror, action: { (label, value) in
             if label != nil {
                 codableProperties[label!] = unwrap(any: value) as AnyObject?
@@ -182,23 +182,23 @@ extension PropertieValues {
         return codableProperties
     }
     
-//    //遍历所有的属性列表，将所有的属性存储到数组中
-//    func getPropertiesWithMirror(mir : Mirror) -> [String] {
-//        var result: [String] = []
-//        if let superMirror = mir.superclassMirror {
-//            result = getPropertiesWithMirror(mir: superMirror)
-//        }
-//        for case let (label?, _) in (mir.children) {
-//            result.append(label)
-//        }
-//        return result
-//    }
+    //    //遍历所有的属性列表，将所有的属性存储到数组中
+    //    func getPropertiesWithMirror(mir : Mirror) -> [String] {
+    //        var result: [String] = []
+    //        if let superMirror = mir.superclassMirror {
+    //            result = getPropertiesWithMirror(mir: superMirror)
+    //        }
+    //        for case let (label?, _) in (mir.children) {
+    //            result.append(label)
+    //        }
+    //        return result
+    //    }
     
     //遍历所有的属性列表，将所有的属性存储到数组中
-    func allProperties() -> [String] {
+    public func allProperties() -> [String] {
         var allProperties = [String]()
         let mirror = Mirror(reflecting: self)
-//        codableProperties = getPropertiesWithMirror(mir: mirror)
+        //        codableProperties = getPropertiesWithMirror(mir: mirror)
         getResultFromMirror(mir: mirror, action: { (label, value) in
             if label != nil {
                 allProperties.append(label!)
@@ -212,7 +212,7 @@ extension PropertieValues {
     ///
     /// - Parameter any: any 类型对象
     /// - Returns: 可选值
-    func unwrap(any: Any) -> Any? {
+    public func unwrap(any: Any) -> Any? {
         let mirror = Mirror(reflecting: any)
         if mirror.displayStyle != .optional {
             return any
@@ -226,13 +226,13 @@ extension PropertieValues {
 }
 
 //MARK: - JSONModel
-class JSONModel: NSObject, JSON, NSCoding, PropertieValues {
+open class JSONModel: NSObject, JSON, NSCoding, PropertieValues {
     
-    override init() {
+    override public init() {
         
     }
     
-    override public var description: String {
+    override open var description: String {
         return toJSONString()
     }
     
@@ -260,5 +260,5 @@ class JSONModel: NSObject, JSON, NSCoding, PropertieValues {
             self.setValue(object, forKey: key)
         }
     }
-
+    
 }
